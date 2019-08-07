080c428b36c645bc5e262d5c302dd825ce9be3a1
cujicujija <facbrizio@gmail.com>
2019-08-07 19:09:14 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import Sidebar from './components/Sidebar';
+import VerticalNav from './components/VerticalNav/VerticalNav';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Sidebar /> 
+      {/* <header className="App-header">
+         
+      </header> */}
+      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/VerticalNav/VerticalNav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './VerticalNav.css';
+
+class VerticalNav extends React.Component {
+
+    render() {
+        return (
+            <div className="VerticalNav-container">
+                <a href="" className="Nav-item">//Apliables logo</a>    
+                <a href="" className="Nav-item">//Home</a>    
+                <a href="" className="Nav-item">//Archivo </a>    
+                <a href="" className="Nav-item">//AdminPanel</a>    
+            </div>
+        )    
+    }
+}
+
+export default VerticalNav;
